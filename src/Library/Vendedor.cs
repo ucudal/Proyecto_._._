1b6f18243747f5e9f465380dc7917c8c4295cb18
Thiago Soca <Thiagosoca22@gmail.com>
2025-11-10 10:17:20 -0300

    Clase Vendedor

---
+++ src/Library/Vendedor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Library
+{
+    public class Vendedor : IUsuario
+    {
+        // Propiedad que indica si el vendedor está activo o no
+        public bool Activo { get; set; }
+
+        // Fecha en la que se creó o registró el vendedor
+        public DateTime FechaCreacion { get; set; }
+
+        // Constructor que inicializa los valores de activo y fecha de creación
+        public Vendedor(bool activo, DateTime fechaCreacion)
+        {
+            Activo = activo;
+            FechaCreacion = fechaCreacion;
+        }
+
+        // Método que devuelve la lista de clientes asignados al vendedor
+        // Por ahora devuelve una lista vacía
+        public List<Cliente> getClientesAsignados()
+        {
+            return new List<Cliente>();
+        }
+    }
+
+}
