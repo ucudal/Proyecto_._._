f879d8a82a6240aa356babecd11f68f4c3a5d5e4
AlexisGM2806 <alexisgimenezmartinez@gmail.com>
2025-11-10 19:47:41 -0300

    Clase correo agregada

---
+++ src/Library/Correo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Library
+{
+    public class Correo : Interaccion
+    {
+        public Correo(DateTime fecha, string descripcion, string notas, bool respondida, string direccion)
+            : base(fecha, descripcion, notas, respondida, direccion)
+        {
+        }
+    }
+}
