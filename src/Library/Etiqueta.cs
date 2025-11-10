2a18611389f3f81a0380e027c6a481fef832390a
Thiago Soca <Thiagosoca22@gmail.com>
2025-11-10 10:08:28 -0300

    clase Etiqueta

---
+++ src/Library/Etiqueta.cs
@@ -0,0 +1,21 @@
+﻿namespace Library
+{
+    public class Etiqueta
+    {
+        // Atributo privado donde guardo el nombre de la etiqueta
+        private string nombre;
+
+        // Constructor que recibe el nombre al crear la etiqueta
+        public Etiqueta(string nombre)
+        {
+            // Asigno el valor del parámetro al atributo de la clase
+            this.nombre = nombre;
+        }
+
+        // Método que devuelve el nombre de la etiqueta
+        public string ObtenerNombre()
+        {
+            return nombre;
+        }
+    }
+}
